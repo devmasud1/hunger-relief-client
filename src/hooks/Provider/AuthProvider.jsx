cf0305661462444d4910daec035391718efac304
devmasud1 <devmahmud.masu1@gmail.com>
2023-11-08 17:32:59 +0600

    set and remove token by user login & logout

--- src/hooks/Provider/AuthProvider.jsx
+++ src/hooks/Provider/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import app from "../Firebase/Firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -59,9 +60,29 @@ const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currUser) => {
-      setUser(currUser);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      const userEmail = currentUser?.email || user?.email;
+      const loggedUser = { email: userEmail };
+
+      setUser(currentUser);
       setLoading(false);
+
+      if (currentUser) {
+        axios
+          .post("https://hunger-relief-server.vercel.app/jwt", loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            console.log("token response", res.data);
+          });
+      }  else {
+        axios.post("https://hunger-relief-server.vercel.app/logout", loggedUser, {
+            withCredentials: true
+        })
+            .then(res => {
+                console.log(res.data);
+            })
+    }
     });
     return () => {
       unsubscribe();
